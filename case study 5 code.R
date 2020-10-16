9ec2c4eea24217faad9d1517c3c5978ad7178f43
Cody Laptop <cjmatek@gmail.com>
2020-10-16 13:38:05 -0600

    before laptop dies hopefully

--- case study 5 code.R
+++ case study 5 code.R
@@ -9,13 +9,15 @@ library(readr)
 gunDeaths <- read_csv(url("https://github.com/fivethirtyeight/guns-data/blob/master/full_data.csv?raw=true"))
 View(gunDeaths)
 
-gunDeaths <- mutate(season = 
-                      case_when(
-                        month %in% c(12,01,02) ~ "Winter"
-                        month %in% c(03,04,05) ~ "Spring"
-                        month %in% c(06,07,08) ~ "Summer"
-                        month %in% c(09,10,11) ~ "Winter"
-                      ))
+gunDeaths <- gunDeaths %>% 
+  mutate(season = 
+            case_when(
+              month %in% c(12,01,02) ~ "Winter",
+              month %in% c(03,04,05) ~ "Spring",
+              month %in% c(06,07,08) ~ "Summer",
+              month %in% c(09,10,11) ~ "Fall"
+              )
+            )
 
 gunDeaths %>% 
   ggplot(aes(x=intent, group = education, fill = education), 
